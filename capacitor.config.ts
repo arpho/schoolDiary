b9692427920f0d5f27ae274d545a770af6c5389a
arpho <damicogiuseppe77@gmail.com>
2025-07-06 18:13:56 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'schoolDiary',
+  webDir: 'www'
+};
+
+export default config;
