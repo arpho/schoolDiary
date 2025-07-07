f248b8582fa1613021fe7cff20d62082f1e54c2b
arpho <damicogiuseppe77@gmail.com>
2025-07-07 11:17:01 +0200

    started implementation of the auth's feature
    copied code from wodlog

---
+++ src/app/shared/models/userModel.ts
@@ -0,0 +1,48 @@
+import { ActivityModel } from "./activityModel";
+
+export class UserModel{
+  setKey(uid: string) {
+  this.key = uid;
+  return this
+  }
+  key = ''
+  birthDate: string|number = ''
+  email = ''
+  firstName = ''
+  lastName = ''
+  password = ''
+  classKey:string = ''
+  phoneNumber = ''
+  role = ''
+  prList: ActivityModel[] = []
+  userName = ''
+  constructor(args?:{}){
+    this.build(args);
+  }
+
+  build(args?:{}){
+    Object.assign(this, args)
+    const pr:ActivityModel[] = []
+Object.entries(this.prList).forEach(([key, value]) => {
+  pr.push(new ActivityModel(value).setKey(key))
+})
+this.prList= pr
+
+    return this
+
+}
+serialize(){
+  return{
+    key: this.key,
+    birthDate: this.birthDate,
+    classKey: this.classKey,
+    email: this.email,
+    firstName: this.firstName,
+    lastName: this.lastName,
+    password: this.password,
+    phoneNumber: this.phoneNumber,
+    role: this.role,
+    userName: this.userName
+  }
+}
+}
---
+++ src/app/shared/services/users.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@angular/core';
+import {
+  Auth,
+  authState,
+  signInAnonymously,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  getAuth,
+  createUserWithEmailAndPassword,
+} from '@angular/fire/auth';
+import { AuthService } from './auth.service';
+import { getDatabase, onValue, push, ref, set } from 'firebase/database';
+import { UserModel } from '../../models/userModel';
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  db = getDatabase();
+  collection = 'userProfile';
+
+  constructor(
+    private auth: Auth,
+
+    private MyAuth: AuthService
+  ) {}
+  isUserAuthenticated(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.auth.onAuthStateChanged((user) => {
+        if (user) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    });
+  }
+
+  getLoggedUser(): Promise<UserModel> {
+    console.log('ciao user seervice');
+    return new Promise((resolve, reject) => {
+      this.MyAuth.getUser().subscribe(async (user) => {
+        if (user) {
+          const loggedUser = await this.getUserByUid(user.uid);
+          resolve(loggedUser);
+        }
+      });
+    });
+  }
+
+  async signupUser(user: UserModel) {
+    const auth = getAuth();
+    const createdUser = await createUserWithEmailAndPassword(
+      auth,
+      user.email,
+      user.password
+    );
+    user.setKey(createdUser.user.uid);
+    const userRef = ref(this.db, `${this.collection}/${user.key}`);
+    const result = set(userRef, user.serialize());
+
+    return result;
+  }
+  logout() {
+    const auth = getAuth();
+    console.log('auth', auth);
+    auth.signOut();
+  }
+
+  getUserByUid(uid: string): Promise<UserModel> {
+    const UsewrRef = ref(this.db, `userProfile/${uid}`);
+    return new Promise((resolve, reject) => {
+      onValue(UsewrRef, (snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          const user = new UserModel(data).setKey(uid);
+          resolve(user);
+        } else {
+          console.log('No user found');
+          reject('no data found');
+        }
+      });
+    });
+  }
+}
