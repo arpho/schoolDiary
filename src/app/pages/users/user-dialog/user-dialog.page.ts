c8a01b174e0c87229631517cbbfffaec837419da
arpho <damicogiuseppe77@gmail.com>
2025-07-16 13:39:38 +0200

    Implements user dialog page wip
    Creates the user dialog page with a form to edit user details.
    
    The form includes fields for first name, last name, username, email, role, phone number, birth date, and classes.
    #TODO add classes/set class store changes, set custom claims
    
    Fetches user data based on the userKey passed via navigation extras and populates the form with the user's information.

--- src/app/pages/users/user-dialog/user-dialog.page.ts
+++ src/app/pages/users/user-dialog/user-dialog.page.ts
@@ -1,38 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { UsersService } from 'src/app/shared/services/users.service';
 import { UserModel } from 'src/app/shared/models/userModel';
 import { signal } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { UsersRole } from 'src/app/shared/models/usersRole';
 @Component({
   selector: 'app-user-dialog',
   templateUrl: './user-dialog.page.html',
   styleUrls: ['./user-dialog.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonSelect,
+    IonSelectOption,
+    ReactiveFormsModule
+  ]
 })
 export class UserDialogPage implements OnInit {
   userKey = "";
 userSignal = signal(new UserModel());
+rolesValue: any[] = [];
+rolesName: string[] = [];
+userForm: FormGroup= new FormGroup({
+  firstName: new FormControl(''),
+  lastName: new FormControl(''),
+  userName: new FormControl(''),
+  email: new FormControl(''),
+  role: new FormControl(''),
+  phoneNumber: new FormControl(''),
+  birthDate: new FormControl(''),
+  classes: new FormControl(''),
+});
+$UsersRole = UsersRole;
   constructor(
     private router: Router,
     private $users: UsersService, 
-  ) { }
+    ) { }
 
   ngOnInit() {
+     const rolesKey = Object.keys(UsersRole);
+     this.rolesValue = Object.values(UsersRole).slice(rolesKey.length/2);
+     console.log("rolesKey", rolesKey);
+     console.log("rolesValue", this.rolesValue);
     const navigation = this.router.getCurrentNavigation();
     this.userKey = navigation?.extras.state?.['userKey'];
-    if(this.userKey){
+    if(this.userKey){   
       this.$users.fetchUser(this.userKey).then((user) => {
         if(user){
-        this.userSignal.set(user);
-      }
+          this.userSignal.set(user);
+          this.userForm.setValue({
+            firstName: user?.firstName,
+            lastName: user?.lastName,
+            userName: user?.userName,
+            email: user?.email,
+            role: user?.role,
+            phoneNumber: user?.phoneNumber,
+            birthDate: user?.birthDate,
+            classes: user?.classes,      
+          });
+        }
       });
     }
-    console.log("userKey", this.userSignal());
+    console.log("user", this.userSignal());
 
   }
-
 }
