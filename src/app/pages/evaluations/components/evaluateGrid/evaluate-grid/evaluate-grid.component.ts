ed4a97449efdff0ae809480cd630f3e84910703b
arpho <damicogiuseppe77@gmail.com>
2025-07-20 18:41:14 +0200

    Displays evaluation grid with indicators
    Implements a component to display the evaluation grid based on selected criteria.
    The grid shows indicators with descriptions and max values.
    It also imports and uses Ionic components.
    Conditionally renders the evaluation grid based on the presence of a grid key.

--- src/app/pages/evaluations/components/evaluateGrid/evaluate-grid/evaluate-grid.component.ts
+++ src/app/pages/evaluations/components/evaluateGrid/evaluate-grid/evaluate-grid.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Grids } from 'src/app/shared/models/grids';
-
+import { IonList, IonItem, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonPopover, IonContent } from "@ionic/angular/standalone";
 @Component({
   selector: 'app-evaluate-grid',
   templateUrl: './evaluate-grid.component.html',
   styleUrls: ['./evaluate-grid.component.scss'],
+  imports: [IonList,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonPopover, IonContent],
 })
 export class EvaluateGridComponent  implements OnInit, OnChanges {
   @Input() grid: Grids = new Grids(); 
