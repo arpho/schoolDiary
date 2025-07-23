6565b9475d259a03e22b784679614d53b30d652f
arpho <damicogiuseppe77@gmail.com>
2025-07-23 15:51:24 +0200

    Improves evaluation PDF component layout
    Enhances the layout of the evaluation PDF component.
    It introduces the teacher's name and a final grade,
    improving readability and information density.
    Adjusts column sizes and adds a section header for indicators.

--- src/app/pages/evaluations/components/evaluation2-pdf/evaluation2-pdf.component.ts
+++ src/app/pages/evaluations/components/evaluation2-pdf/evaluation2-pdf.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewEncapsulation, input, OnInit, signal, inject } from '@angular/core';
+import { Component, ViewEncapsulation, input, OnInit, signal, inject, computed } from '@angular/core';
 import { Evaluation } from 'src/app/shared/models/evaluation';
 import { IonGrid, IonRow, IonCol } from "@ionic/angular/standalone";
 import { UserWieverComponent } from "src/app/shared/components/user-wiever/user-wiever.component";
 import { ClassViewerComponent } from "src/app/shared/components/class-wiever/class-wiever.component";
 import { DatePipe } from '@angular/common';
+import { Indicatore } from 'src/app/shared/models/indicatore';
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'app-evaluation2-pdf',
@@ -22,7 +23,11 @@ export class Evaluation2PdfComponent  implements OnInit {
 
   evaluation=input<Evaluation>();
   evaluationData=signal<Evaluation>(new Evaluation());
-
+ votazione= computed(() => {
+const max =  this.evaluationData().grid?.indicatori.reduce((acc, indicatore) => Number(acc) + Number(indicatore.valore), 0);
+const voto = this.evaluationData().grid?.indicatori.reduce((acc, indicatore) => Number(acc) + Number(indicatore.voto), 0);
+  return `${voto}/${max}`
+ })
   constructor(
   ) { }
 
