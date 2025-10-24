656e3245a124a62ca2253d5a80e171c30355a499
arpho <damicogiuseppe77@gmail.com>
2025-10-24 12:00:17 +0200

    Adds students with Pdp component
    Adds a new component to display students with personalized disability plans (PDPs).

---
+++ src/app/pages/classes/components/students-with-pd-p/students-with-pd-p.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { StudentsWithPdPComponent } from './students-with-pd-p.component';
+
+describe('StudentsWithPdPComponent', () => {
+  let component: StudentsWithPdPComponent;
+  let fixture: ComponentFixture<StudentsWithPdPComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StudentsWithPdPComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(StudentsWithPdPComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/classes/components/students-with-pd-p/students-with-pd-p.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-students-with-pd-p',
+  templateUrl: './students-with-pd-p.component.html',
+  styleUrls: ['./students-with-pd-p.component.scss'],
+})
+export class StudentsWithPdPComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
