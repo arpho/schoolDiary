8616ca09540763ed4fd332704740b88d858d966b
arpho <damicogiuseppe77@gmail.com>
2025-11-20 16:11:54 +0100

    Prevents event propagation on evaluation list click
    Stops the propagation of the click event when opening the evaluation list.
    This prevents unintended actions on parent elements.
    
    Also, allows to pass a color to the toaster

--- src/app/shared/services/toaster.service.ts
+++ src/app/shared/services/toaster.service.ts
@@ -5,17 +5,18 @@ import { ToastController } from '@ionic/angular';
   providedIn: 'root'
 })
 export class ToasterService {
-  showToast(data:{message?: string, duration?: number , position: "bottom" | "top" | "middle"}) {
-    this.presentToast(data);
+  showToast(data:{message?: string, duration?: number , position: "bottom" | "top" | "middle"},color?: string) {
+    this.presentToast(data,color);
   }
   private toastCtrl = inject(ToastController);
 
 
-  async presentToast(data:{message?: string, duration?: number , position?: "bottom" | "top" | "middle"}) {
+  async presentToast(data:{message?: string, duration?: number , position?: "bottom" | "top" | "middle"},color?: string) {
     const toast = await this.toastCtrl.create({
       message: data.message,
       duration: data.duration,
-      position: data.position
+      position: data.position,
+      color: color
     });
     toast.present();
     setTimeout(() => {
